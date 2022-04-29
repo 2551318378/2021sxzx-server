8ccf84fd9c854b6233352ae1325d6b538eb41314
2551318378 <49371215+2551318378@users.noreply.github.com>
2022-04-29 10:06:30 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,12 +9,7 @@ if(process.env.NODE_ENV === 'dev'){
   MONGO_CONFIG = {
    url       : 'mongodb://127.0.0.1:27017/sxzx'
   }
-}else if(process.env.NODE_ENV === 'pro'){
-  REDIS_CONFIG = {
-    host     : '8.134.73.52',
-    port     : '6379',
-    password : 'hgc16711'
-  }
+}
   MONGO_CONFIG = {
     url      : 'mongodb://root2:Hgc16711@8.134.73.52:27017/sxzx'
   }
